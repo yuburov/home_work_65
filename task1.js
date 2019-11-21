94d2e4eccf5d8f4049b2e98777ff1041cec2de92
Yuburov <yubur.aziz@gmail.com>
2019-11-21 13:40:02 +0600

    сделано первое задание

---
+++ task1.js
@@ -0,0 +1,10 @@
+let myArray = ['молоко','пиво','пиво','молоко','молоко'];
+
+for(let i=0; i<myArray.length; i++) {
+    if (myArray[i] == 'молоко') {
+        console.log('Хорошо');  
+    }
+    else {
+        console.log('Плохо');
+    }
+}
